f50c0e47da5ff04381bc0ee2355bf2586cf917cf
nuriyevn <nuriyevn@gmail.com>
2018-12-12 11:04:43 +0200

    activating Goods model

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# pk 1
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=500)
+
+class Goods(models.Model):
+    author = models.CharField(max_length=250)
+    good_title = models.CharField(max_length=250)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
--- killolx/settings.py
+++ killolx/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'goods.apps.GoodsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
